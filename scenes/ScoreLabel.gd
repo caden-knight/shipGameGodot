19efa3ae00ad975ad25ef40c93c1eba0cf55448c
Caden Morrison <cadenknight@gmail.com>
2023-06-22 00:21:06 -0600

    got score working
    Finally the score works!

--- scenes/ScoreLabel.gd
+++ scenes/ScoreLabel.gd
@@ -1,9 +1,5 @@
 extends Label
 
-@onready var enemy = get_parent().get_node("Enemy")
-func _ready():
-	print(enemy.get_signal_list())
-#	self.connect(en)
-	
-func _on_update_score(msg: String):
-	self.text = msg
+func _process(_delta):
+	self.text = str(Global.score)
+
--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -3,12 +3,6 @@ extends CharacterBody2D
 const SPEED: float = 200.0
 var msgString: String = "poop"
 var addScore: bool = false
-signal update_score(msg: String)
-
-
-
-
-
 
 func _physics_process(delta):
 	var _collision_info = move_and_collide(Vector2.DOWN.normalized() * SPEED * delta)
@@ -18,12 +12,7 @@ func _physics_process(delta):
 		
 		var body := _collision_info.get_collider()
 		print("YOU COLLIDED WITH " + body.name)
+		Global.score += 1
 		self.queue_free()
 		body.free()
-	
-	if addScore:
-		msgString = "hey"
-		update_score.emit(msgString)
-		print("add")
-		addScore = false
 
